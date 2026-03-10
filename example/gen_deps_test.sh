b6a6a21a12c382ef071d6c2d43b01d257b7ae0d2
Martin Nowak <code@dawg.eu>
2026-03-11 00:00:19 +0100

    feat: Implement Makefile-driven automation with dynamic dependency generation

---
+++ example/gen_deps_test.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bashunit
+
+set_up() {
+    ENV=dev-eu
+    NSTACKS=0
+    source gen_deps.sh $ENV $NSTACKS
+}
+
+test_branch_pattern() {
+    re=$(along_branch_re path/to/dir)
+    assert_string_starts_with '^(path/to/dir/|path/to/|path/|)[^/]+' $re
+    assert_matches $re$ path/to/dir/leaf.tf
+    assert_matches $re$ root.tf
+    assert_matches $re$ path/branch.tfvars
+    assert_not_matches $re$ ./root.tfvars
+    assert_not_matches $re$ path/to/dir2/leaf.tf
+    assert_not_matches $re$ path/to/dir/sub/below.tf
+}
+
+test_env_match() {
+    ENV='dev-eu-fr'
+    assert_same 3 $(env_match 'dev' $ENV)
+    assert_same 2 $(env_match 'eu' $ENV)
+    assert_same 1 $(env_match 'fr' $ENV)
+    assert_same 3 $(env_match 'dev-eu' $ENV)
+    assert_same 2 $(env_match 'eu-fr' $ENV)
+
+    set +e
+    for name in 'dev-fr' 'dev-' 'dev-e' 'eu-' 'eu-f' 'fr-'; do
+        env_match "$name" "$ENV"
+        assert_unsuccessful_code
+    done
+}
